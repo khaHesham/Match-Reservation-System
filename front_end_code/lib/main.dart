93cccd485f599c7be387af7111b3a9a9e717b03a
Abdelaziz Salah <abdelaziz132001@gmail.com>
2023-12-07 02:19:14 +0200

    adding the fundmentals for the app

--- front_end_code/lib/main.dart
+++ front_end_code/lib/main.dart
@@ -14,12 +14,21 @@ class MyApp extends StatelessWidget {
       title: 'EPL Reservo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.white, inversePrimary: Colors.black),
+            background: const Color.fromARGB(207, 0, 0, 0),
+            seedColor: Colors.white,
+            inversePrimary: Colors.black),
         useMaterial3: true,
         textTheme: const TextTheme(
           titleMedium: TextStyle(
             fontSize: 48,
             fontWeight: FontWeight.w500,
+            fontFamily: 'RubikBubbles',
+            color: Colors.red,
+          ),
+          bodySmall: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.w500,
+            fontFamily: 'Whisper',
             color: Colors.red,
           ),
         ),
@@ -52,11 +61,43 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actionsIconTheme: const IconThemeData(color: Colors.amber),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          ),
+          // circular avatar contains text to sign in
+          GestureDetector(
+            onTap: () {
+              //! TODO: implement sign in
+              print('Sign in');
+            },
+            child: Container(
+              margin: const EdgeInsets.only(right: 30),
+              // it should be rounded
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Theme.of(context).colorScheme.background,
+              ),
+              child: const Text(
+                'Log In',
+                style: TextStyle(
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+          ),
+        ],
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Row(mainAxisSize: MainAxisSize.min, children: [
           CircleAvatar(
             child: Image.asset(
-              '../lib/assets/imgs/epl-logo.png',
+              './assets/imgs/epl-logo.png',
               fit: BoxFit.cover,
             ),
           ),
@@ -65,29 +106,30 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           Text(
             widget.title,
-            style: const TextStyle(color: Colors.amber),
+            style: const TextStyle(
+                color: Colors.amber, fontFamily: 'RubikBubbles'),
           )
         ]),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: const Center(
+        child: Card(
+          elevation: 30,
+          shadowColor: Color.fromARGB(57, 255, 193, 7),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+                'Welcome to EPL Reservo, Here You can enjoy the best experience with our fantastic league\n Order Your Ticket NOW!'),
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        tooltip: 'reserve your ticket now!',
+        child: GestureDetector(
+            child: const Icon(
+          Icons.add,
+        )),
       ),
     );
   }
